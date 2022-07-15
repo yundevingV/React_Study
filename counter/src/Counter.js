bd3c7b7c3efe390ef1bc0ac46a5da843f8d4c653
ys0222 <ggdbstjd0222@gmail.com>
2022-07-15 15:36:25 +0900

    0715

---
+++ counter/src/Counter.js
@@ -0,0 +1,33 @@
+import React, {useReducer} from 'react'
+
+function reducer(state,action) {
+    switch (action.type) {
+        case 'INCREMENT' :
+            return state +1;
+        case 'DECREMENT' :
+            return state -1;
+        default :
+            return state;
+    }
+}
+
+function Counter() {
+    const [number,dispatch] = useReducer(reducer,0)
+
+    const onIncrease = () => {
+        dispatch({type : 'INCREMENT'})
+    }
+    const onDecrease = () => {
+        dispatch({type : 'DECREMENT'})
+    }
+
+    return (
+        <div>
+            <h1>{number}</h1>
+            <button onClick={onIncrease}>+</button>
+            <button onClick={onDecrease}>-</button>
+        </div>
+    )
+}
+
+export default Counter
