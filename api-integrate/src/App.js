e9b9161ff9536989a72c796bc56ee7c5f3542feb
ys0222 <ggdbstjd0222@gmail.com>
2022-07-29 21:16:30 +0900

    0729

--- api-integrate/src/App.js
+++ api-integrate/src/App.js
@@ -1,8 +1,10 @@
 import Users from './Users'
-
+import React from 'react';
 function App() {
   return (
-     <Users/>
+    <>
+     <Users />
+    </>
   );
 }
 
--- api-integrate/src/Users.js
+++ api-integrate/src/Users.js
@@ -1,41 +1,41 @@
-import axios from 'axios'
-import useAsync from './useAsync'
-import React,{useState} from 'react'
-import User from './User'
-
+import React, { useState } from 'react';
+import axios from 'axios';
+import { useAsync } from 'react-async';
+import User from './User';
 
 async function getUsers() {
-    const response = await axios.get(
-        'https://jsonplaceholder.typicode.com/users'
-    )
-    return response.data
+  const response = await axios.get(
+    'https://jsonplaceholder.typicode.com/users'
+  );
+  return response.data;
 }
+
 function Users() {
-    // const [users,setUsers] = useState(null)
-    // const [loading,setLoading] = useState(false)
-    // const [error,setError] = useState(null)
+  const [userId, setUserId] = useState(null);
+  const { data: users, error, isLoading, run } = useAsync({
+    deferFn: getUsers
+  });
 
-    // //요청결과 , 로딩상태, 에러
-    const [userId, setUserId] =useState(null)
-    const [state,refetch] = useAsync(getUsers,[],true)
-    const {loading,data :users,error} = state
-
-    if (loading) return <div> 로딩중 ... </div>
-    if (error) return <div> 에러가 발생! </div>
-    if (!users) return <button onClick={refetch}>불러오기</button>;
-
-    return (
-        <ul>
-            {users.map(user => (
-                <li key={user.id}
-                    onClick={() => setUserId(user.id)}
-                    style={{cursor : 'pointer'}}>
-                    {user.username} ({user.name})
-                </li>  ))}
-                <button onClick={refetch}>다시 불러오기 </button>
-                {userId && <User id={userId} />}
-        </ul>
-    )
+  if (isLoading) return <div>로딩중..</div>;
+  if (error) return <div>에러가 발생했습니다</div>;
+  if (!users) return <button onClick={run}>불러오기</button>;
+  return (
+    <>
+      <ul>
+        {users.map(user => (
+          <li
+            key={user.id}
+            onClick={() => setUserId(user.id)}
+            style={{ cursor: 'pointer' }}
+          >
+            {user.username} ({user.name})
+          </li>
+        ))}
+      </ul>
+      <button onClick={run}>다시 불러오기</button>
+      {userId && <User id={userId} />}
+    </>
+  );
 }
 
-export default Users
+export default Users;
